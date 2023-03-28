77a6311c9dbc24c6f32a422a2d5d47a49673b0bc
Aleksandr Zubov <alexanderzubov@icloud.com>
2023-03-28 21:31:34 +0200

    initial_commit_dev

---
+++ Challenges/Challenge_2-1/Challenge_2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge_2-1
+//
+//  Created by user228564 on 3/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
